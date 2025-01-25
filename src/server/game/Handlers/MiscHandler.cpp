d3a547705d418026d3fd2a6c3924894609d16b7d
Julio A. Leyva Osorio <104047767+Snet-Cuba@users.noreply.github.com>
2025-01-25 07:02:27 -0500

    WhoList_Staff

--- src/server/game/Handlers/MiscHandler.cpp
+++ src/server/game/Handlers/MiscHandler.cpp
@@ -397,7 +397,24 @@ void WorldSession::HandleWhoOpcode(WorldPacket& recvData)
             continue;
         }
 
-        data << target.GetPlayerName();                   // player name
+        std::string playerName = ""; //temp storage
+
+        //definition of playerName depending on the (player/GM)'s rank
+        if (target.GetSecurity() == 1) // Si el rango es igual a 1
+        {
+            playerName = "|cffFFFFFF[|r|cffFA8258Staff|cffffffff]|r  " + target.GetPlayerName();
+        }
+        else if (target.GetSecurity() == 4) // Si el rango es igual a 4
+        {
+            playerName = "|cffFFFFFF[|r|cffFA8258Owner|cffffffff]|r  " + target.GetPlayerName();
+        }
+        else // Para otros casos
+        {
+            playerName = target.GetPlayerName();
+        }
+
+        //data << target.GetPlayerName();                   // player name
+        data << playerName;                               // player name - with Staff indicator
         data << target.GetGuildName();                    // guild name
         data << uint32(lvl);                              // player level
         data << uint32(class_);                           // player class
